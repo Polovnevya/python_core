088b0456f3a7ae0e4e4df6d0ce7991aaa85bbdb8
Yuriy <PolovnevYA@gmail.com>
2020-06-27 02:03:03 +0300

    done!

--- Lessons/Lesson_5/Homework_6.py
+++ Lessons/Lesson_5/Homework_6.py
@@ -11,11 +11,9 @@
 
 def read_user_file(filename):
     """Открывает файл, читает его построчно, добавляет строки в список file_lines
-
     :param filename: Принимает имя файла вместе с расширением
     :return: возаращает список прочитаных строк file_lines, при неудаче возвращает ошибку IOError
     """
-
     try:
         with open(filename, "r", encoding="utf-8") as f_user_obj:
             return f_user_obj.readlines()
@@ -24,16 +22,28 @@ def read_user_file(filename):
 
 
 def create_subjects_dict(user_string_list):
+    def my_replace(my_string):
+        """функция создана как обертка над стандартным методом строк replace, для использования в функции map
+        принимает строку, если находит суффиксы  - убирает их
+        :param my_string: строка
+        :return: возвращает строку без суффиксов
+        """
+        my_string = my_string.replace("(пр)", "").replace("(л)", "").replace("(лаб)", "")
+        return my_string
+
     subjects = {}
     for el in user_string_list:
-        b = [x for x in el.split() if x != "-"]
-        c = map(replace("(пр)", ""), b)
-        for my_dig in tmp:
-            a = my_dig
-        subjects[el.split()[0]] = el.split()[1]
+        # генерируем список, если элемент равен "-" - отбрасываем этот элемент
+        my_list = [x for x in el.split() if x != "-"]
+        # прогоняем список через функцию mapб используюя свою обертку над методом строк replace - убираем суффиксы
+        my_list_w_o_suffix = list(map(my_replace, my_list))
+        # заполняем словарь
+        subjects[my_list_w_o_suffix[0]] = sum(map(int, my_list_w_o_suffix[1:]))
+    return subjects
 
 
 user_file_name = "text_6.txt"
 result = create_subjects_dict(read_user_file(user_file_name))
-
-result2 = result
+# печатаем наш словарь, получаем итерар ключ + значение через метод items()
+for key, value in result.items():
+    print(f"{key}  {value}")
