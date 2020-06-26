843d2c953bdbafbbe1363791b9f3a5fe089eb02a
Yuriy <PolovnevYA@gmail.com>
2020-06-27 01:09:39 +0300

    not done yet, just for fun

--- Lessons/Lesson_5/Homework_6.py
+++ Lessons/Lesson_5/Homework_6.py
@@ -8,3 +8,32 @@
 #                                         Физкультура:   —   30(пр)   —
 # Пример словаря: {“Информатика”: 170, “Физика”: 40, “Физкультура”: 30}
 
+
+def read_user_file(filename):
+    """Открывает файл, читает его построчно, добавляет строки в список file_lines
+
+    :param filename: Принимает имя файла вместе с расширением
+    :return: возаращает список прочитаных строк file_lines, при неудаче возвращает ошибку IOError
+    """
+
+    try:
+        with open(filename, "r", encoding="utf-8") as f_user_obj:
+            return f_user_obj.readlines()
+    except IOError:
+        return IOError
+
+
+def create_subjects_dict(user_string_list):
+    subjects = {}
+    for el in user_string_list:
+        b = [x for x in el.split() if x != "-"]
+        c = map(replace("(пр)", ""), b)
+        for my_dig in tmp:
+            a = my_dig
+        subjects[el.split()[0]] = el.split()[1]
+
+
+user_file_name = "text_6.txt"
+result = create_subjects_dict(read_user_file(user_file_name))
+
+result2 = result
