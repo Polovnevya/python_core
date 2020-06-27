e1825d22cbbc9b3c32d52a9db1b0a5a1076db9a7
Yuriy <PolovnevYA@gmail.com>
2020-06-27 11:17:31 +0300

    almost done

--- Lessons/Lesson_5/Homework_7.py
+++ Lessons/Lesson_5/Homework_7.py
@@ -10,3 +10,58 @@
 # Пример json-объекта:
 # [{"firm_1": 5000, "firm_2": 3000, "firm_3": 1000}, {"average_profit": 2000}]
 # Подсказка: использовать менеджер контекста.
+
+
+def read_user_file(user_filename):
+    """Открывает файл, читает его построчно, добавляет строки в список file_lines
+
+    :param user_filename: Принимает имя файла вместе с расширением
+    :return: возаращает список прочитаных строк file_lines, при неудаче возвращает ошибку IOError
+    """
+    try:
+        with open(user_filename, "r", encoding="utf-8") as f_user_obj:
+            file_lines = f_user_obj.readlines()
+            return file_lines
+    except IOError:
+        return IOError
+
+
+def convert_list_of_string_2_list_of_dict(user_result):
+    """производит конвертацию списка стров в список словарей
+        :param user_result: принимает список строк
+        Ключи словаря
+        "Firm_name": Название фирмы
+        "Type_of_ownership": тип собственности
+        "Earnings": Выручка
+        "Costs": Затраты
+    :return: возвращает список словарей при успехе, иначе ValueError
+    """
+    firm = {с}
+    firms = []
+    for line in user_result:
+        try:
+            firm["Firm_name"] = line.split()[0]
+            firm["Type_of_ownership"] = line.split()[1]
+            firm["Earnings"] = float(line.split()[2])
+            firm["Costs"] = float(line.split()[3].replace(r"\n", ""))
+            firms.append(firm.copy())
+        except ValueError:
+            return ValueError
+    return firms[:]
+
+
+filename = "text_7.txt"
+result = read_user_file(filename)
+if result != IOError:
+    list_of_firms = convert_list_of_string_2_list_of_dict(result)
+    profit_firms = [firm for firm in list_of_firms if (firm["Earnings"] - firm["Costs"]) > 0]
+    profit_dict = {}
+    average_profit = {"average_profit": None}
+    firms_sum = 0
+    for firm in profit_firms:
+        profit_dict[firm["Firm_name"]] = (firm["Earnings"] - firm["Costs"])
+        firms_sum = firms_sum + (firm["Earnings"] - firm["Costs"])
+    average_profit = firms_sum / len(profit_firms)
+
+    #
+a = 123
