69b284a3b6b15a79ffc1dcba56c262fa44df27d0
Yuriy <PolovnevYA@gmail.com>
2020-06-27 13:43:02 +0300

    DONE!

--- Lessons/Lesson_5/Homework_4.py
+++ Lessons/Lesson_5/Homework_4.py
@@ -8,6 +8,9 @@
 # Новый блок строк должен записываться в новый текстовый файл.
 
 
+from googletrans import Translator
+
+
 def read_user_file(user_filename):
     """Открывает файл читает построчно
     :param user_filename: Принимает имя файла вместе с расширением
@@ -23,7 +26,35 @@ def read_user_file(user_filename):
         return IOError
 
 
+def get_translate(eng_list):
+    try:
+        my_translator = Translator()
+        ru_words = []
+        trans_result = (my_translator.translate(eng_list, src='en', dest='ru'))
+        for i, word in enumerate(trans_result):
+            ru_word = f"{word[0].text.title()} - {i + 1}"
+            ru_words.append(ru_word)
+        return ru_words
+    except:
+        return "Error"
+
+
+def write_2_file(user_filename, user_string):
+    """Записть в файл реализована через print
+    Если возникает ошибка доступа к файлу возвращает ошибку "IOError"
+    :param user_filename: Имя файла для записи, по умолчанию к имени добавляет расширение .txt
+    :param user_string: Строка которая будет записываться в файл
+    :return: Возвращает ошибку при неудачном обращении к файлу
+    """
+    try:
+        with open(f"{user_filename.split('.')[0]}_ru.txt", "w", encoding="utf-8") as f_user_obj:
+            for el in user_string:
+                print(el, file=f_user_obj)
+    except IOError:
+        return IOError
+
+
 filename = "text_4.txt"
 result = read_user_file(filename)
-
-a = 12
+ru_result = get_translate(result)
+write_2_file(filename,ru_result)
