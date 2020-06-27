df501cea84efb8ca21a40c42c8b76e8b084adf9c
Yuriy <PolovnevYA@gmail.com>
2020-06-27 13:13:46 +0300

    almost done

--- Lessons/Lesson_5/Homework_4.py
+++ Lessons/Lesson_5/Homework_4.py
@@ -7,15 +7,23 @@
 # При этом английские числительные должны заменяться на русские.
 # Новый блок строк должен записываться в новый текстовый файл.
 
-def read_user_file(user_filename):
-    """Открывает файл, читает его построчно, добавляет строки в список file_lines
 
+def read_user_file(user_filename):
+    """Открывает файл читает построчно
     :param user_filename: Принимает имя файла вместе с расширением
     :return: возаращает список прочитаных строк file_lines, при неудаче возвращает ошибку IOError
     """
+    file_lines = []
     try:
         with open(user_filename, "r", encoding="utf-8") as f_user_obj:
-            file_lines = f_user_obj.readlines()
+            for line in f_user_obj:
+                file_lines.append(line.replace("\n", "").split("-"))
             return file_lines
     except IOError:
         return IOError
+
+
+filename = "text_4.txt"
+result = read_user_file(filename)
+
+a = 12
