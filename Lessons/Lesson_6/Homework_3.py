86ef8565566efbcf375be450236e03f77bf1d320
Yuriy <PolovnevYA@gmail.com>
2020-06-28 22:02:12 +0300

    done

--- Lessons/Lesson_6/Homework_3.py
+++ Lessons/Lesson_6/Homework_3.py
@@ -7,3 +7,26 @@
 # получения полного имени сотрудника (get_full_name) и дохода с учетом премии (get_total_income).
 # Проверить работу примера на реальных данных
 # (создать экземпляры класса Position, передать данные, проверить значения атрибутов, вызвать методы экземпляров).
+
+
+class Worker:
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {"wage": wage, "bonus": bonus}
+
+
+class Position(Worker):
+
+    def get_full_name(self):
+        return f"{self.surname} {self.name}"
+
+    def get_total_income(self):
+        return self._income["wage"] + self._income["bonus"]
+
+
+my_position = Position("Yuriy", "Polovnev", "Admin", 35000, 15000)
+
+print(f"Employee {my_position.get_full_name()}")
+print(f"Income {my_position.get_total_income()}")
