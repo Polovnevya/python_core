a43f255ed7a5b0818d84067f6db45155a2a94d05
Yuriy <PolovnevYA@gmail.com>
2020-06-29 22:51:11 +0300

    done

--- Lessons/Lesson_6/Homework_5.py
+++ Lessons/Lesson_6/Homework_5.py
@@ -5,3 +5,37 @@
 # В каждом из классов реализовать переопределение метода draw.
 # Для каждого из классов метод должен выводить уникальное сообщение.
 # Создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра.
+
+
+class Stationery:
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        print(f"Запуск отрисовки {self.title}")
+
+
+class Pen(Stationery):
+    def draw(self):
+        print(f"Выбрана ручка {self.title}")
+
+
+class Pencil(Stationery):
+    def draw(self):
+        print(f"Выбран карандаш {self.title}")
+
+
+class Handle(Stationery):
+    def draw(self):
+        print(f"Выбран маркер {self.title}")
+
+
+my_stationery = Stationery("школьная готовальня")
+my_pen = Pen("красного цвета")
+my_pencil = Pencil("пунктирнй")
+my_handle = Handle("тройной жирности")
+
+my_stationery.draw()
+my_pen.draw()
+my_pencil.draw()
+my_handle.draw()
