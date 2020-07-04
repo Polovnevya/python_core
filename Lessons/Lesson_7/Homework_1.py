7ee6b5547411de465a73f3b88727a6934e4ce1d1
Yuriy <PolovnevYA@gmail.com>
2020-07-04 16:24:47 +0300

    done

--- Lessons/Lesson_7/Homework_1.py
+++ Lessons/Lesson_7/Homework_1.py
@@ -24,13 +24,25 @@ class Matrix:
 
     def __add__(self, other):
         my_matrix_str = ""
+        my_matrix_1 = []
+        my_matrix_2 = []
         for r_index, row in enumerate(self.l_matrix):
             my_matrix_str = "" if (my_matrix_str == "") else my_matrix_str + "\n"
             for c_index, column in enumerate(self.l_matrix[r_index]):
-                my_matrix_str += f"{(self.l_matrix[r_index][c_index] + other.l_matrix[r_index][c_index]): ^ 5}"
-        return my_matrix_str
+                my_matrix_str += f"{(self.l_matrix[r_index][c_index] + other.l_matrix[r_index][c_index])},"
+        # жуткий костыль, ибо невнимательно прочитал задание. возвращал строку вместо нового экземпляра класса
+        my_matrix_1 = my_matrix_str.split(sep="\n")
+        for i in my_matrix_1:
+            my_matrix_2.append(i.split(sep=","))
+        my_matrix_1=[]
+        for i in my_matrix_2:
+            my_matrix_1.append(list(map(int, i[:len(i) - 1])))
+        #можно выдохнуть, ужасы закончились
+        return Matrix(my_matrix_1)
 
 
 my_matrix_1 = Matrix([[1, 2, 7], [3, 4, 8], [5, 6, 9]])
 my_matrix_2 = Matrix([[10, 12, 17], [13, 14, 18], [15, 16, 19]])
+my_matrix_3 = my_matrix_1 + my_matrix_2
+print(f"проверяем что у нас возвращает результат сложения 2х матриц - "type(my_matrix_3))
 print(f"{my_matrix_1}\n+\n{my_matrix_2}\n=\n{my_matrix_1 + my_matrix_2}")
