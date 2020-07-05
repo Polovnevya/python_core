6d908df266aa490d842488e0aa43c7c1b0a88f7e
Yuriy <PolovnevYA@gmail.com>
2020-07-05 21:07:02 +0300

    done

--- Lessons/Lesson_8/Homework_2.py
+++ Lessons/Lesson_8/Homework_2.py
@@ -2,3 +2,22 @@
 # Проверьте его работу на данных, вводимых пользователем.
 # При вводе пользователем нуля в качестве делителя программа должна корректно
 # обработать эту ситуацию и не завершиться с ошибкой.
+
+
+class MyOwnErr(Exception):
+    def __init__(self, err_text):
+        self.err_text = err_text
+
+
+print(f"Программа производит обработку деления на ноль")
+dividend = None
+divider = None
+try:
+    dividend = int(input("Введите делимое "))
+    divider = int(input("Введите делитель "))
+    if divider == 0:
+        raise MyOwnErr("На ноль делить нельзя!")
+except ValueError:
+    print(f"\nОшибка преобразования к числу")
+except MyOwnErr as err:
+    print(err)
