08df6a5b0de9eb5f9d629fa6d13c6fbd9ee87472
Yuriy <PolovnevYA@gmail.com>
2020-07-09 21:22:37 +0300

    almost done

--- Lessons/Lesson_8/Homework_7.py
+++ Lessons/Lesson_8/Homework_7.py
@@ -3,3 +3,32 @@
 # Проверьте работу проекта, создав экземпляры класса (комплексные числа) и
 # выполнив сложение и умножение созданных экземпляров.
 # Проверьте корректность полученного результата.
+
+
+class Complex:
+    def __init__(self, x: str):
+        self.x = self.convert(x)
+
+    @staticmethod
+    def convert(x):
+        try:
+            x = complex(x)
+            return x
+        except ValueError:
+            return f"Ошибка преобразования {x} к комплексному числу"
+
+    def __add__(self, other):
+        re = int(self.x.real + other.x.real)
+        im = int(self.x.imag + other.x.imag)
+        return Complex(f"{re}{im}j")
+
+    def __mul__(self, other):
+        re = int(self.x.real * other.x.real)
+        im = int(self.x.imag * other.x.imag)
+        return Complex(f"{re}{im}j")
+
+
+my_num1 = Complex("-3j")
+my_num2 = Complex("-2-3j")
+my_num3 = my_num1 + my_num2
+my_num4 = my_num1 * my_num2
